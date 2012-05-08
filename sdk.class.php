ccd42a3c3e7e7b898bf6f346b54e5a14c8ab24b0
James McParlane <james@massive.com.au>
2012-05-08 14:49:54 +1000

    Latest changes.

--- sdk.class.php
+++ sdk.class.php
@@ -14,6 +14,7 @@
  * permissions and limitations under the License.
  */
 
+ 
 
 /*%******************************************************************************************%*/
 // CORE DEPENDENCIES
